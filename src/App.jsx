dd3dddb55110b5f5b175caaf87b25f36f234508c
Sotatek-Tungle2 <tung.le2@sotatek.com>
2024-07-22 17:57:10 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <p className="text-blue-600/100">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
